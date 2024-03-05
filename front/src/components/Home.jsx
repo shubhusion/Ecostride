4d48b1919271d99ebba2bfa0fff1c9e1c9de97be
shrivastavaditya <113035198+shrivastavaditya@users.noreply.github.com>
2024-03-06 02:25:42 +0530

    Home page basic structure defined with a side bar, calendar and task to do list

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,18 +1,27 @@
+// home.jsx or app.jsx
 import React from 'react';
 import Calendar from './Calendar';
+import Sidebar from './Sidebar';
 import ActionSections from './Action';
-import './home.css';
 
-function Home() {
+import './home.css'; // Add this import for your main styling
+
+const Home = () => {
   return (
-    <div className="home-container">
-    <h1>EcoStride : Sustainability Living Tracker</h1>
-      <div className="sections-container">
-        <ActionSections />
-        <Calendar />
+    <>
+      <div className="main-container">
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="content-container">
+          <div className="sections-container">
+            <ActionSections />
+            <Calendar />
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default Home;
--- front/src/components/Sidebar.jsx
+++ front/src/components/Sidebar.jsx
@@ -1,11 +1,33 @@
+// sidebar.jsx
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faUsers, faBullseye, faLightbulb, faShoppingCart, faGlobe, faEarth } from '@fortawesome/free-solid-svg-icons';
+import './sidebar.css';
 
-function Sidebar() {
+const Sidebar = () => {
+  const sidebarOptions = [
+    { icon: faUser, title: 'Profile' },
+    { icon: faUsers, title: 'Community' },
+    { icon: faBullseye, title: 'Goals' },
+    { icon: faLightbulb, title: 'Feedback' },
+    { icon: faShoppingCart, title: 'Cart' },
+  ];
+
   return (
-    <div className="sidebar-container">
-      {/* Your sidebar content here */}
+    <div className="sidebar">
+      <div className="sidebar-logo">
+        <FontAwesomeIcon icon={faEarth} size="2x" />
+      </div>
+      <div className="sidebar-options">
+        {sidebarOptions.map((option, index) => (
+          <div key={index} className="sidebar-option">
+            <FontAwesomeIcon icon={option.icon} />
+            <span>{option.title}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Sidebar;
