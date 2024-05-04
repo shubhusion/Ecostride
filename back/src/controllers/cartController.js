04dfca912632b2b747d183039d12d0a0152d0cc4
shubhusion <shubham27.sharma03@gmail.com>
2024-05-04 18:35:17 +0530

    Add to Cart API Created

--- back/src/controllers/cartController.js
+++ back/src/controllers/cartController.js
@@ -1,9 +1,19 @@
 const Cart = require('../models/cart');
+const Product = require('../models/product');
 
 // Add an item to the cart
 exports.addToCart = async (req, res) => {
   try {
-    const { productId, quantity, price } = req.body;
+    const { productId, quantity } = req.body;
+    
+    // Find the product by productId
+    const product = await Product.findOne({productId});
+    console.log(product)
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    const price = product.price;
     let cart = await Cart.findOne({ userId: req.user._id });
 
     if (!cart) {
@@ -19,7 +29,6 @@ exports.addToCart = async (req, res) => {
 
       if (existingItemIndex !== -1) {
         cart.items[existingItemIndex].quantity += quantity;
-        cart.items[existingItemIndex].price = price;
       } else {
         cart.items.push({ productId, quantity, price });
       }
@@ -33,6 +42,44 @@ exports.addToCart = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+// Remove an item from the cart
+exports.removeFromCart = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const cart = await Cart.findOne({ userId: req.user._id });
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    const itemIndex = cart.items.findIndex(
+      (item) => item.productId.toString() === productId
+    );
+
+    if (itemIndex === -1) {
+      return res.status(404).json({ message: 'Item not found in cart' });
+    }
+
+    // Find the product by productId to get the price
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    const itemPrice = product.price;
+    const itemQuantity = cart.items[itemIndex].quantity;
+    cart.totalAmount -= itemPrice * itemQuantity;
+    cart.items.splice(itemIndex, 1);
+
+    await cart.save();
+
+    res.json(cart);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
 
 // Get the user's cart
 exports.getUserCart = async (req, res) => {
--- back/src/models/cart.js
+++ back/src/models/cart.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const cartItemSchema = new mongoose.Schema({
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  productId: { type: Number, ref: 'Product', required: true },
   quantity: { type: Number, required: true, default: 1 },
   price: { type: Number, required: true },
 });
