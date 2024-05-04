fd6a38f9c46f8fbc17caed6d1389252af63e7c51
shubhusion <shubham27.sharma03@gmail.com>
2024-05-05 00:41:56 +0530

    Order API Created

--- back/src/models/order.js
+++ back/src/models/order.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema(
   {
-    orderId: { type: Number, required: true, unique: true },
+    orderId: { type: Number, unique: true },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     products: [
       {
-        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        productId: { type: Number, ref: 'Product', required: true },
         quantity: { type: Number, required: true },
         price: { type: Number, required: true },
       },
     ],
     totalAmount: { type: Number, required: true },
-    status: { type: String, required: true, default: 'pending' },
+    status: { type: String, default: 'pending' },
     shippingAddress: { type: String, required: true },
     paymentMethod: { type: String, required: true },
   },
